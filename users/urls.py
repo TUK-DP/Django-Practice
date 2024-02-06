2781f535e10db057c66d14c36ca7d263b9c3d525
이원찬 <dnjscks4257@naver.com>
2024-02-06 19:11:03 +0900

    [chore] Users findALL api 작성

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.findAll),
 ]
 # users/ 요청이 오면 view.py의 index함수를 실행!
--- users/views.py
+++ users/views.py
@@ -1,7 +1,23 @@
+from django.forms import model_to_dict
 from django.http import HttpResponse
 from django.core.handlers.wsgi import WSGIRequest
 from django.views.decorators.csrf import csrf_exempt
 
+from users.models import Users
+
 
 def index(request: WSGIRequest):
     return HttpResponse("Users Index Page")
+
+
+def findAll(request: WSGIRequest) -> HttpResponse:
+    # Users.objects.all() => select * from users
+    objects_all = Users.objects.all()
+
+    # Convert QuerySet to List
+    l = list(map(model_to_dict, objects_all))
+
+    print(l)
+
+    if request.method == "GET":
+        return HttpResponse(l)
