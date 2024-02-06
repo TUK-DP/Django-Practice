31fa8c2bee4e9e0cf88f800aca43de6563558a98
이원찬 <dnjscks4257@naver.com>
2024-02-06 19:01:52 +0900

    [chore] username, email이 있는 User Model 생성

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from config.basemodel import BaseModel
+
+
 # Create your models here.
+class User(BaseModel):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.username
