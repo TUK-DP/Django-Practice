c4250862add5a4d8d5a55698a2511a5af69ab982
rvbear <rvbear2000@gmail.com>
2024-02-10 02:35:46 +0900

    [chore] User에 password 속성 추가

--- users/models.py
+++ users/models.py
@@ -7,6 +7,9 @@ from config.basemodel import BaseModel
 class User(BaseModel):
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=128, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.username
