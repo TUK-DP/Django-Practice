76320ef8ec18383cdd58f2eec74fbfbfe2ea17b9
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-03-06 16:28:12 +0900

    [fix] import, 라인 정리

--- diary/views.py
+++ diary/views.py
@@ -1,18 +1,16 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from diary.models import Diary, Sentences, Quizs
 from diary.serializers import *
 from users.models import User
-
-from drf_yasg.utils import swagger_auto_schema
-
 from .textrank import TextRank, make_quiz
+
 
 class DiaryView(APIView):
     def get(self, request: WSGIRequest) -> HttpResponse:
@@ -36,7 +34,7 @@ class DiaryView(APIView):
 
 
 class WriteView(APIView):
-    @swagger_auto_schema(operation_description="일기 작성", request_body=WriteRequest, responses={"201":'작성 성공'})
+    @swagger_auto_schema(operation_description="일기 작성", request_body=WriteRequest, responses={"201": '작성 성공'})
     def post(self, request):
         serializer = WriteRequest(data=request.data)
 
@@ -45,7 +43,8 @@ class WriteView(APIView):
             try:
                 user = User.objects.get(id=user_id)
             except User.DoesNotExist:
-                return JsonResponse({'isSuccess': False, 'message': '사용자를 찾을 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse({'isSuccess': False, 'message': '사용자를 찾을 수 없습니다.'},
+                                    status=status.HTTP_400_BAD_REQUEST)
 
             diary = Diary.objects.create(user=user, title=serializer.validated_data.get('title'))
 
@@ -56,15 +55,17 @@ class WriteView(APIView):
             memory = TextRank(content=content)
             question, answer = make_quiz(memory, keyword_size=5)
 
-            Quizs.objects.bulk_create([Quizs(question=q, answer=a, sentence=sentence) for q, a in zip(question, answer)])
+            Quizs.objects.bulk_create(
+                [Quizs(question=q, answer=a, sentence=sentence) for q, a in zip(question, answer)])
 
-            return JsonResponse({'isSuccess': True, 'result': SentenceSimpleSerializer(sentence).data}, status=status.HTTP_201_CREATED)
-        
+            return JsonResponse({'isSuccess': True, 'result': SentenceSimpleSerializer(sentence).data},
+                                status=status.HTTP_201_CREATED)
+
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UpdateView(APIView):
-    @swagger_auto_schema(operation_description="일기 수정", request_body=UpdateRequest, responses={"201":'작성 성공'})
+    @swagger_auto_schema(operation_description="일기 수정", request_body=UpdateRequest, responses={"201": '작성 성공'})
     def post(self, request):
         serializer = UpdateRequest(data=request.data)
 
@@ -74,7 +75,8 @@ class UpdateView(APIView):
             try:
                 user = User.objects.get(id=user_id)
             except User.DoesNotExist:
-                return JsonResponse({'isSuccess': False, 'message': '사용자를 찾을 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse({'isSuccess': False, 'message': '사용자를 찾을 수 없습니다.'},
+                                    status=status.HTTP_400_BAD_REQUEST)
 
             try:
                 deleteDiary = Diary.objects.get(id=diary_id)
@@ -89,16 +91,20 @@ class UpdateView(APIView):
                 memory = TextRank(content=content)
                 question, answer = make_quiz(memory, keyword_size=5)
 
-                Quizs.objects.bulk_create([Quizs(question=q, answer=a, sentence=sentence) for q, a in zip(question, answer)])
+                Quizs.objects.bulk_create(
+                    [Quizs(question=q, answer=a, sentence=sentence) for q, a in zip(question, answer)])
 
-                return JsonResponse({'isSuccess': True, 'result': SentenceSimpleSerializer(sentence).data}, status=status.HTTP_201_CREATED)
+                return JsonResponse({'isSuccess': True, 'result': SentenceSimpleSerializer(sentence).data},
+                                    status=status.HTTP_201_CREATED)
             except Diary.DoesNotExist:
-                return JsonResponse({'isSuccess': False, 'message': '일기를 찾을 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse({'isSuccess': False, 'message': '일기를 찾을 수 없습니다.'},
+                                    status=status.HTTP_400_BAD_REQUEST)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GetDiarybyUserView(APIView):
-    @swagger_auto_schema(operation_description="유저의 일기 조회", query_serializer=GetUserRequest, responses={"200":DiarySerializer})
+    @swagger_auto_schema(operation_description="유저의 일기 조회", query_serializer=GetUserRequest,
+                         responses={"200": DiarySerializer})
     def get(self, request):
         serializer = GetUserRequest(data=request.query_params)
 
@@ -109,13 +115,13 @@ class GetDiarybyUserView(APIView):
                 serializer = DiarySerializer(diaries, many=True)
                 return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
             except User.DoesNotExist:
-                return JsonResponse({'isSuccess' : False, 'message' : '사용자를 찾을 수 없습니다.'}, status=status.HTTP_201_CREATED)
-        
+                return JsonResponse({'isSuccess': False, 'message': '사용자를 찾을 수 없습니다.'}, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
+
+
 class GetQuizView(APIView):
-    @swagger_auto_schema(operation_description="일기회상 퀴즈", query_serializer=GetDiaryRequest, responses={"200":"퀴즈"})
+    @swagger_auto_schema(operation_description="일기회상 퀴즈", query_serializer=GetDiaryRequest, responses={"200": "퀴즈"})
     def get(self, request):
         serializer = GetDiaryRequest(data=request.query_params)
 
@@ -124,16 +130,17 @@ class GetQuizView(APIView):
             try:
                 diary = Diary.objects.get(id=diary_id)
             except Diary.DoesNotExist:
-                return JsonResponse({'isSuccess': False, 'message': '해당 일기를 찾을 수 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
+                return JsonResponse({'isSuccess': False, 'message': '해당 일기를 찾을 수 없습니다.'},
+                                    status=status.HTTP_404_NOT_FOUND)
 
             sentences = diary.sentences.all()
             quizs = []
 
             for sentence in sentences:
                 quizs.extend(sentence.quizs.all())
-            
+
             serializer = QuizSerializer(quizs, many=True)
 
             return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
-        
+
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
