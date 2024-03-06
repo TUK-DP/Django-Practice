2686b955c67b4a5120830ab0e0e02ddaf503059d
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-03-06 16:45:40 +0900

    [fix] 안사용하는 ViewClass 제거

--- diary/urls.py
+++ diary/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import DiaryView, WriteView, GetDiarybyUserView, GetQuizView, UpdateView
+from django.urls import path
+from .views import WriteView, GetDiarybyUserView, GetQuizView, UpdateView
 
 urlpatterns = [
-    path('', DiaryView.as_view()),
     path('write', WriteView.as_view()),
     path('list', GetDiarybyUserView.as_view()),
     path('quiz', GetQuizView.as_view()),
--- diary/views.py
+++ diary/views.py
@@ -1,36 +1,12 @@
-from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from diary.serializers import *
 from users.models import User
 from .textrank import TextRank, make_quiz
-
-
-class DiaryView(APIView):
-    def get(self, request: WSGIRequest) -> HttpResponse:
-        findDiaries = Diary.objects.all()
-        serializer = DiarySerializer(findDiaries, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    @csrf_exempt
-    def post(self, request: WSGIRequest) -> HttpResponse:
-        data = JSONParser().parse(request)
-        request = DiaryCreateRequest(data=data)
-        is_valid = request.is_valid()
-        print(request.data)
-        if is_valid:
-            findUser = User.objects.get(pk=request.data["userId"])
-            newDiary = request.to_diary(findUser)
-
-            return JsonResponse(newDiary.data, status=201)
-        else:
-            return JsonResponse(DiarySerializer(data=data).errors, status=400)
 
 
 class WriteView(APIView):
