5821d6f2de9c4e24048dcbeb746bbc119dabf595
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-03-26 22:14:15 +0900

    [hotfix] Swagger 동작을 위한 url 패턴 추가

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,8 @@ from django.urls import re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,3 +23,5 @@ urlpatterns = [
 
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
