914553506e332815d4cc2c1b887ac238284ab60e
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-04-25 21:46:00 +0900

    [chore] User 모델에 refresh_token 필드 추가

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class User(BaseModel):
     password = models.CharField(max_length=128, null=True)
     username = models.CharField(max_length=20, null=True)
     birth = models.DateField(null=True)
+    refresh_token = models.CharField(max_length=128, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
