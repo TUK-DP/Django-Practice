bfa370231eff1eaf964d9518d8dbfbc28fd0d29d
rvbear <rvbear2000@gmail.com>
2024-05-01 17:39:39 +0900

    [hotfix] url 오타 수정

--- diary/keyword_urls.py
+++ diary/keyword_urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .keyword_views import *
 
 urlpatterns = [
-    path('/diray/<int:diaryId>', GetKeywordView.as_view()),
+    path('/diary/<int:diaryId>', GetKeywordView.as_view()),
     path('/<int:keywordId>/image', KeywordImgSaveView.as_view()),
 ]
