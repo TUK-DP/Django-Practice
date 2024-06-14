9fbed83312c66911afb77decf4db5d2984e0032c
이원찬 <dnjscks4257@naver.com>
2024-06-14 20:02:47 +0900

    [feat] 자동로그인 테스트 코드 추가

---
+++ users/tests/test_user_auto_login.py
@@ -0,0 +1,58 @@
+import json
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from users.models import User
+from users.token_handler import create_token
+
+# Create your tests here.
+
+test_user_data = {
+    "username": "test1",
+    "nickname": "test1",
+    "email": "test1@test1.com",
+    "password": "test1",
+    "birth": "2021-01-01"
+}
+
+
+class TestUserAutoLogin(APITestCase):
+    def setUp(self):
+        new_user = User.objects.create(**test_user_data)
+        self.token_serializer = create_token(userId=new_user.id)
+        new_user.refresh_token = self.token_serializer.data.get("RefreshToken")
+        new_user.save()
+
+    def test_when_success_user_auto_login(self):
+        find_user = User.objects.first()
+
+        response = self.client.get(
+            f'/api/users/{find_user.id}/auto/login',
+            headers={
+                "AccessToken": self.token_serializer.data.get("AccessToken"),
+                "RefreshToken": self.token_serializer.data.get("RefreshToken")
+            }
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.data['isSuccess'])
+
+        self.assertJSONEqual(
+            json.dumps(response.data['result']['token']),
+            json.dumps(create_token(userId=response.data['result']['user']['id']).data)
+        )
+
+    def test_when_fail_user_auto_login(self):
+        find_user = User.objects.first()
+
+        response = self.client.get(
+            f'/api/users/{find_user.id}/auto/login',
+            headers={
+                "AccessToken": self.token_serializer.data.get("AccessToken"),
+                "RefreshToken": "wrong_refresh_token"
+            }
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertFalse(response.data['isSuccess'])
--- users/tests/test_validator.py
+++ users/tests/test_validator.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 from rest_framework.exceptions import ValidationError
 
 from users.models import User
-from users.validator import not_exist_user_nickname, not_exist_user_email, validate_login, exist_user_email
+from users.token_handler import validate_user_id_token, create_token, decode_token
+from users.token_serializer import TokenSerializer
+from users.validator import *
 
 test_user_data = {
     "username": "test1",
@@ -15,7 +17,8 @@ test_user_data = {
 
 class ValidatorTest(TestCase):
     def setUp(self):
-        User.objects.create(**test_user_data)
+        self.new_user = User.objects.create(**test_user_data)
+        self.token = create_token(userId=self.new_user.id)
 
     def test_not_exist_nickname(self):
         self.assertIsNone(not_exist_user_nickname(nickname='unique_nickname'))
@@ -32,3 +35,35 @@ class ValidatorTest(TestCase):
     def test_validate_login(self):
         self.assertIsNone(validate_login(test_user_data['email'], test_user_data['password']))
         self.assertRaises(ValidationError, validate_login, 'unique_email', 'unique_password')
+
+    def test_exist_user_id(self):
+        self.assertIsNone(exist_user_id(user_id=User.objects.first().id))
+        self.assertRaises(ValidationError, exist_user_id, user_id=0)
+
+    def test_decode_token(self):
+        actual_access_token = self.token.data.get('AccessToken')
+        actual_refresh_token = self.token.data.get('RefreshToken')
+
+        is_valid, message, access_token = decode_token(actual_access_token)
+        self.assertTrue(is_valid)
+        self.assertEqual(access_token['userId'], str(self.new_user.id))
+
+        is_valid, message, refresh_token = decode_token(actual_refresh_token)
+        self.assertTrue(is_valid)
+        self.assertEqual(refresh_token['userId'], str(self.new_user.id))
+
+    def test_validate_user_id_token(self):
+        actual_access_token = self.token.data.get('AccessToken')
+        actual_refresh_token = self.token.data.get('RefreshToken')
+
+        _, _, decoded_access_token = decode_token(actual_access_token)
+        _, _, decoded_refresh_token = decode_token(actual_refresh_token)
+
+        is_valid, _ = validate_user_id_token(decoded_access_token, self.new_user.id)
+        self.assertTrue(is_valid)
+
+        is_valid, _ = validate_user_id_token(decoded_refresh_token, self.new_user.id)
+        self.assertTrue(is_valid)
+
+        is_valid, _ = validate_user_id_token({"userId": str(self.new_user.id)}, 0)
+        self.assertFalse(is_valid)
