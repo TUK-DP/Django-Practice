be1a28433b512c02ad0283cc19a4230a07b57e8f
이원찬 <dnjscks4257@naver.com>
2024-06-14 19:34:07 +0900

    [refactor] user 회원가입 테스트 코드에 회원가입시 사용하는 validator 테스트 코드 추가

--- users/tests/test_user_sign_up.py
+++ users/tests/test_user_sign_up.py
@@ -1,8 +1,10 @@
+from rest_framework.exceptions import ValidationError
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from users.models import User
+from users.validator import not_exist_user_nickname, not_exist_user_email
 
 test_user_data = {
     "username": "test1",
@@ -13,17 +15,6 @@ test_user_data = {
 }
 
 
-class TestUserCreate(TestCase):
-    def test_user_create(self):
-        new_user = User.objects.create(**test_user_data)
-
-        self.assertEqual(new_user.username, 'test1')
-        self.assertEqual(new_user.nickname, 'test1')
-        self.assertEqual(new_user.email, 'test1@test1.com')
-        self.assertEqual(new_user.password, 'test1')
-        self.assertEqual(new_user.birth, '2021-01-01')
-
-
 class TestUserSignUp(APITestCase):
     def test_user_sign_up(self):
         response = self.client.post('/api/users/signup', test_user_data)
@@ -32,3 +23,16 @@ class TestUserSignUp(APITestCase):
         self.assertEqual(response.data['isSuccess'], True)
         self.assertEqual(response.data['message'], 'OK')
         self.assertTrue('id' in response.data['result'])
+
+
+class TestValidator(TestCase):
+    def setUp(self):
+        User.objects.create(**test_user_data)
+
+    def test_not_exist_nickname(self):
+        self.assertIsNone(not_exist_user_nickname(nickname='unique_nickname'))
+        self.assertRaises(ValidationError, not_exist_user_nickname, nickname=test_user_data['nickname'])
+
+    def test_not_exist_user_email(self):
+        self.assertIsNone(not_exist_user_email(email='unique_email'))
+        self.assertRaises(ValidationError, not_exist_user_email, email=test_user_data['email'])
