412ec4fae71ce0398447142ffe4f2d56dda8cade
이원찬 <dnjscks4257@naver.com>
2024-06-14 18:43:53 +0900

    [refactor] user_sign_up test 코드 id 종속되지 않게 수정

--- users/tests/test_user_sign_up.py
+++ users/tests/test_user_sign_up.py
@@ -1,24 +1,21 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from rest_framework import status
+from rest_framework.test import APITestCase
 
 from users.models import User
 
+test_user_data = {
+    "username": "test1",
+    "nickname": "test1",
+    "email": "test1@test1.com",
+    "password": "test1",
+    "birth": "2021-01-01"
+}
 
-# Create your tests here.
 
-class UserSignUpTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.test_data = {
-            "username": "test1",
-            "nickname": "test1",
-            "email": "test1@test1.com",
-            "password": "test1",
-            "birth": "2021-01-01"
-        }
-
+class TestUserCreate(TestCase):
     def test_user_create(self):
-        new_user = User.objects.create(**self.test_data)
+        new_user = User.objects.create(**test_user_data)
 
         self.assertEqual(new_user.username, 'test1')
         self.assertEqual(new_user.nickname, 'test1')
@@ -26,16 +23,12 @@ class UserSignUpTest(TestCase):
         self.assertEqual(new_user.password, 'test1')
         self.assertEqual(new_user.birth, '2021-01-01')
 
+
+class TestUserSignUp(APITestCase):
     def test_user_sign_up(self):
-        response = self.client.post('/api/users/signup', self.test_data)
-
-        new_user_id = len(User.objects.all()) + 1
-
+        response = self.client.post('/api/users/signup', test_user_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        response = response.json()
-
-        self.assertEqual(response['isSuccess'], True)
-        self.assertEqual(response['message'], 'OK')
-
-        self.assertEqual(response['result']['id'], new_user_id)
+        self.assertEqual(response.data['isSuccess'], True)
+        self.assertEqual(response.data['message'], 'OK')
+        self.assertTrue('id' in response.data['result'])
